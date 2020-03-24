ae8573f0de095b1f9893ae6e8b0ba0fc47ce31b9
David Ariando <dariando@ufl.edu>
2020-03-24 15:15:17 -0400

    [SYNC] daily backup

--- SHELL/send_rbf.sh
+++ SHELL/send_rbf.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 quartus_cpf -c ../QUARTUS-SoC/output_files/DE1_SOC_Linux_FB.sof ./soc_system.rbf
-scp ./soc_system.rbf root@129.22.143.88:/media/root/5459-A1D61/soc_system.rbf
+#scp ./soc_system.rbf root@129.22.143.88:/media/root/5459-A1D61/soc_system.rbf
 #scp ./soc_system.rbf root@dajo-de1soc:/media/root/5459-A1D63/soc_system.rbf
+scp ./soc_system.rbf root@192.168.137.10:/media/root/5459-A1D62/soc_system.rbf
